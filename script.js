38beec2c89c13d5621d5bbb7b69f964ff1b0166e
Grant Ferment <grantferment@gmail.com>
2020-11-12 21:17:32 -0500

    linked js to html

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 // $("#searchBtn").on("click", function(event){
 // event.PreventDefault();
 
-var APIkey = "81bcb345a0607fbd12d0daf0e6a57fd3";
-var userCity = "charlotte"; //$("#searchCity").val().trim();
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=charlotte&appid=81bcb345a0607fbd12d0daf0e6a57fd3";
-console.log(queryURL)
+// var APIkey = "81bcb345a0607fbd12d0daf0e6a57fd3";
+// var userCity = "charlotte"; //$("#searchCity").val().trim();
+// var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=charlotte&appid=81bcb345a0607fbd12d0daf0e6a57fd3";
+// console.log(queryURL)
 $.ajax({
     url: "https://api.openweathermap.org/data/2.5/weather?q=charlotte&appid=81bcb345a0607fbd12d0daf0e6a57fd3",
     method: "GET"
@@ -13,5 +13,5 @@ $.ajax({
     // $("#cityWeather").text(JSON.stringify(response))
     console.log(response)
 })
-console.log(queryURL)
+console.log(url)
 // })
